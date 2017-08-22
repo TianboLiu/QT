7d104bd2fae7f0b177046c6ecdd4385156912a22
User <user@UsermatoMacBook-Pro-2.local>
2017-08-22 14:40:58 -0400

    test

---
+++ pycode/tools/Lsidis.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env/ python
+import numpy as np
+import os, sys
+
+import lhapdf
+
+pdfs = lhapdf.mkPDF("CJ15lo", 0)
+ffpion = lhapdf.mkPDF("DSSFFlo", 0211)
+
+
+def Test():
+    print "xf(x): x = 0.1, Q = 3.0"
+    print pdfs.xfxQ(2, 0.1, 3.0)
+    print pdfs.xfxQ(1, 0.1, 3.0)
+    print "zD(z): z = 0.2, Q = 3.0"
+    print ffpion.xfxQ(2, 0.2, 3.0)
+    print ffpion.xfxQ(1, 0.2, 3.0)
+    return
+
+
+
+
+if __name__ == "__main__":
+    sys.exit()
