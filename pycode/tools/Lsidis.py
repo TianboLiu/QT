ed122cfbdcc4b4989d784280444fc9ce326514e0
User <user@UsermatoMacBook-Pro-2.local>
2017-08-22 14:52:17 -0400

    add

--- pycode/tools/Lsidis.py
+++ pycode/tools/Lsidis.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env/ python
 import numpy as np
 import os, sys
+from scipy import pi as PI
+from scipy import sin, cos, exp, log
 
 import lhapdf
 
 pdfs = lhapdf.mkPDF("CJ15lo", 0)
 ffpion = lhapdf.mkPDF("DSSFFlo", 211)
-
 
 def Test():
     print "xf(x): x = 0.1, Q = 3.0"
