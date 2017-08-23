43fc74d95c6195a97014d7ca72e9541c27b63473
User <user@UsermatoMacBook-Pro-2.local>
2017-08-23 11:26:39 -0400

    .

--- pycode/tools/Fitter.py
+++ pycode/tools/Fitter.py
@@ -5,8 +5,8 @@ from scipy.optimize import minimize
 import numpy as np
 from Lsidis import *
 
-def LOADDATA(filename, oldset = np.array([])):
-    dataset = np.loadtxt(filename, skiprows = 20)
+def LOADDATA(filename, oldset = np.array([]).reshape(0,8)):
+    dataset = np.loadtxt(filename, skiprows = 19)
     newset = np.vstack((oldset, dataset))
     return newset
 
