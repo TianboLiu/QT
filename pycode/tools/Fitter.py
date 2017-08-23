81244a17d0d1c3bd09bcc51b80193480d62b94f3
User <user@UsermatoMacBook-Pro-2.local>
2017-08-23 12:04:32 -0400

    a

--- pycode/tools/Fitter.py
+++ pycode/tools/Fitter.py
@@ -16,8 +16,7 @@ def chi2(para):
     global datalist
     sum = 0.0
     for item in datalist:
-        print item[4]
-        if item[4] < 1.5:#Q2
+        if item[4] < 1.5 and item[5] < 0.038 and item[6] < 0.2:#Q2
             print item
             sum += (model_0(x = item[5], Q2 = item[4], z = item[6], Pt = item[7], target = "proton", par = para) - item[1])**2 / (item[2]**2 + item[3]**2)
     return sum
