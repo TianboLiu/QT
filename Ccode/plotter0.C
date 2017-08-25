0e3a9abb2a52491158d4502cfcbbfde82256042f
User <user@UsermatoMacBook-Pro-2.local>
2017-08-25 10:34:25 -0400

    update

--- Ccode/plotter0.C
+++ Ccode/plotter0.C
@@ -50,8 +50,36 @@ int main(const int argc, const char * argv[]){
     hB->SetMinimum(0.0);
 
     double tmp, ptmax, chi2;
+    TLatex latex;
+    latex.SetTextFont(22);
+    latex.SetTextSize(0.05);
+    latex.SetTextAlign(12);
     TGraph * g0;
     int Clist[6] = {1, 4, 2, 5, 6, 7};
+
+    c0->Divide(2,3);
+    c0->cd(1);
+    c0->SetLeftMargin(0.15);
+    c0->SetBottomMargin(0.15);
+    hB->DrawClone("axis");
+
+    ifstream fs1("path/gallery/ptcut_model0_z1.0_ptmin0.1.dat");
+    for (int i = 0; i < 3; i++)
+      fs1.getline(ltmp, 300);
+    int Q2line1[6] = {18, 15, 13, 11, 11, 12};
+       
+    for (int i = 0; i < 6; i++){
+      g0 = new TGraph(Q2line1[i]);
+      for (int j = 0; j < Q2line1[i]; j++){
+	fs1 >> tmp >> tmp >> ptmax >> tmp >> chi2 >> tmp >> tmp >> tmp >> tmp;
+	g0->SetPoint(j, ptmax, chi2);
+      }
+      fs1.getline(ltmp, 300);
+      SetGraph(g0, 1, Clist[i], 2.0, 20, Clist[i], 0.5);
+      g0->DrawClone("lpsame");
+      g0->Delete();
+    }
+    
 
     c0->Divide(2,3);
     c0->cd(1);
