e2bb30f5534d093d575f6d5a61b2d514d9c6ed9a
User <user@UsermatoMacBook-Pro-2.local>
2017-09-05 10:19:29 -0400

    A

--- Ccode/evol0.C
+++ Ccode/evol0.C
@@ -88,9 +88,11 @@ int main(const int argc, const char * argv[]){
 
 
   if (opt == 2){//evolution
+
+    TMDEVOL::Initialize();
     TMDEVOL::C_factor = & TMDEVOL::C_factor_Model0;
     TMDEVOL::kt_model0 = 0.720;
-    cout << TMDEVOL::F_bspace(2, 0.0957, 0.1, sqrt(1.82), 1.82, 1.82) << endl;
+    cout << TMDEVOL::F_kspace(2, 0.0957, 0.1, sqrt(1.82), 1.82, 1.82) << endl;
 
 
 
