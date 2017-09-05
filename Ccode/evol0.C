023d7a70ba83ce6e0ca1810a52a6355b91c7f189
User <user@UsermatoMacBook-Pro-2.local>
2017-09-05 10:01:17 -0400

    A

--- Ccode/evol0.C
+++ Ccode/evol0.C
@@ -85,6 +85,16 @@ int main(const int argc, const char * argv[]){
     }
     fclose(fs);
   }
-    
+
+
+  if (opt == 2){//evolution
+    TMDEVOL::C_factor = & TMDEVOL::C_factor_model0;
+    TMDEVOL::kt_model0 = 0.720;
+    cout << F_kspace(2, 0.0957, 0.1, sqrt(1.82), 1.82, 1.82) << endl;
+
+
+
+  }
+  
   return 0;
 }
