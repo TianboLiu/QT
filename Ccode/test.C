70aecb67f861f7e09f22d569a3a0d0cf52874bde
Tianbo Liu <tl190@grads-dd.internal.phy.duke.edu>
2017-09-15 10:50:49 -0400

    a

--- Ccode/test.C
+++ Ccode/test.C
@@ -2,36 +2,11 @@
 #include "tmdevol.h"
 
 int main(const int argc, const char * argv[]){
-/*
-  double xf[13];
-  DIS::zFF(xf, 0.2, 2.0, "pi+");
-  for (int i = 0; i < 13; i++)
-    cout << xf[i] << endl;
 
-  cout << endl;
+  double result = Test();
 
-  DIS::zFF(xf, 0.2, 2.0, "pi-");
-  for (int i = 0; i < 13; i++)
-    cout << xf[i] << endl;
+  cout << result << endl;
 
-  DIS::zFF(xf, 0.2, 2.0, "pi0");
-  for (int i = 0; i < 13; i++)
-    cout << xf[i] << endl;
-*/
-
-  double var[4] = {0.42, 9.21, 0.42, 0.304};
-  double par[2] = {0.5, 0.2};
-
-  SIDIS::FUUT = &SIDIS::Model_FUUT_0;
- 
-  cout << SIDIS::Multiplicity(var, par, "proton", "pi+") << endl;
-
-  TMDEVOL::Initialize();
-
-  cout << TMDEVOL::bstar(0.5) << endl;
-
-
-  cout << TMDEVOL::F_bspace(2, 0.1, 0.5, 2.0, 2.0*2.0, 2.0*2.0) << endl;
 
   return 0;
 }
